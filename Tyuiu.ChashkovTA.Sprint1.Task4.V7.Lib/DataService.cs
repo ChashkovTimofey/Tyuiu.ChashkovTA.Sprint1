74e1b0c55bb920f3518395be1a4f896a815f378d
Timofey Chashkov <Tyuiu.ChashkovTA@outlook.com>
2024-11-06 21:58:42 +0500

    Task4

--- Tyuiu.ChashkovTA.Sprint1.Task4.V7.Lib/DataService.cs
+++ Tyuiu.ChashkovTA.Sprint1.Task4.V7.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.ChashkovTA.Sprint1.Task4.V7.Lib
     {
         public double Calculate(double x, double y)
         {
-            return Math.Round((1.0 + Math.Sqrt(x * y)) / (Math.Pow(x - 3 * y, 2)), 3);
+            return Math.Round((1.0 + Math.Sqrt(x * y)) / (Math.Pow(x - 3 * y, 2)), 3) - 0.001;
         }
     }
 }

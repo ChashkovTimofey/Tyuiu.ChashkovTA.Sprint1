af1b297a16500f648fef96a1d46f1330a3cca435
Timofey Chashkov <Tyuiu.ChashkovTA@outlook.com>
2024-11-06 21:26:50 +0500

    Task3

---
+++ Tyuiu.ChashkovTA.Sprint1.Task3.V6.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.ChashkovTA.Sprint1.Task3.V6.Lib
+{
+    public class DataService : ISprint1Task3V6
+    {
+        public double TravelCost(double distance, double gasFlow, double gasPrice)
+        {
+            return Math.Round(distance * 2 / 100 * gasFlow * gasPrice, 3);
+
+        }
+        
+    }
+}
---
+++ Tyuiu.ChashkovTA.Sprint1.Task3.V6/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.ChashkovTA.Sprint1.Task3.V6.Lib;
+namespace Tyuiu.ChashkovTA.Sprint1.Task3.V6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #1 | Выполнил: Чашков Тимофей Александрович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #6                                                            *");
+            Console.WriteLine("* Выполнил: Чашков Тимофей Александрович | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.TravelCost(134, 8.5, 74.04);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}

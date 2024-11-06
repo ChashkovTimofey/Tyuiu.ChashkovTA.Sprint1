27d4130ffc4d2074f1fa1ad41f4c352ac462840c
Timofey Chashkov <Tyuiu.ChashkovTA@outlook.com>
2024-11-06 22:24:54 +0500

    Task5

---
+++ Tyuiu.ChashkovTA.Sprint1.Task5.V2/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.ChashkovTA.Sprint1.Task5.V2.Lib;
+namespace Tyuiu.ChashkovTA.Sprint1.Task5.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #1 | Выполнил: Чашков Тимофей Александрович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #2                                                            *");
+            Console.WriteLine("* Выполнил: Чашков Тимофей Александрович | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x;
+
+            Console.WriteLine("Введите значение x:");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.FahrenheitToСelsius(x);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}

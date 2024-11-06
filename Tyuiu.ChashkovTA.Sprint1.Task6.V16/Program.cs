d4dcbb3264f6f355694ee984c012f5f21da9483c
Timofey Chashkov <Tyuiu.ChashkovTA@outlook.com>
2024-11-06 22:50:35 +0500

    Task6

---
+++ Tyuiu.ChashkovTA.Sprint1.Task6.V16/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.ChashkovTA.Sprint1.Task6.V16.Lib;
+namespace Tyuiu.ChashkovTA.Sprint1.Task6.V16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #1 | Выполнил: Чашков Тимофей Александрович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнил: Чашков Тимофей Александрович | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.YOURFUNCTION();
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}

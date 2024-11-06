bf0bdd8caced7158faa9ccf1fde47e41a1b6664c
Timofey Chashkov <Tyuiu.ChashkovTA@outlook.com>
2024-11-06 20:42:41 +0500

    Task2

---
+++ Tyuiu.ChashkovTA.Sprint1.Task2.V19/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.ChashkovTA.Sprint1.Task2.V19.Lib;
+namespace Tyuiu.ChashkovTA.Sprint1.Task2.V19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #1 | Выполнил: Чашков Тимофей Александрович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #19                                                             *");
+            Console.WriteLine("* Выполнил: Чашков Тимофей Александрович | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите расстояние в дюймах:");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            Console.WriteLine("Расстояние в метрах = " + ds.ConvertInchToKm(x));
+            Console.ReadKey();
+        }
+    }
+}
